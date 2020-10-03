64a0a267cf63ffd14f8698a32558a33e9c35801b
tensorknower69 <62006583+tensorknower69@users.noreply.github.com>
2020-10-03 14:31:16 +0800

    improve test

--- test/Test.hs
+++ test/Test.hs
@@ -1,20 +1,16 @@
 {-# LANGUAGE TypeApplications #-}
 
-import Control.Monad
-import Test.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
 import Text.HTML.Scalpel
 import Text.HTML.Scalpel.FreeProxyList
 
-tests :: Test
-tests = TestList
-	[ TestLabel "scrap https://free-proxy-list.net" testScrap
-	]
-
-testScrap :: Test
-testScrap = TestCase $ do
-	Just proxies <- scrapeURL @String freeProxyListUrl freeProxyListScraper
-	print proxies
+test :: TestTree
+test = testCase ("scrap " <> freeProxyListUrl) $ do
+	result <- scrapeURL @String freeProxyListUrl freeProxyListScraper
+	case result of
+		Just proxies -> print proxies
+		Nothing -> error $ "unable to download from " <> freeProxyListUrl
 
 main :: IO ()
-main = do
-	void $ runTestTT tests
+main = defaultMain test
